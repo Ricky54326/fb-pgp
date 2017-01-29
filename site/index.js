5261974251e88b9c71adc1c3034885d3cac49069
Ricky Mutschlechner <mutschlechne@wisc.edu>
2017-01-28 22:22:42 -0600

    added attempt at API call from node

--- site/index.js
+++ site/index.js
@@ -5,7 +5,7 @@ var express       = require('express'),
     FB            = require('fb'),
     http          = require('http'),
     path          = require('path'),
-
+    Step          = require('step'),
     config        = require('./secrets.js');
 
     
@@ -49,8 +49,53 @@ var root = function (req, res) {
 };
 
 var login_callback = function (req, res, next) {
-    console.log(req, res);
-    res.send('wat');
+    var code            = req.query.code;
+
+    if(req.query.error) {
+        // user might have disallowed the app
+        return res.send('login-error ' + req.query.error_description);
+    } else if(!code) {
+        return res.redirect('/');
+    }
+
+    Step(
+        function exchangeCodeForAccessToken() {
+            FB.napi('oauth/access_token', {
+                client_id:      FB.options('appId'),
+                client_secret:  FB.options('appSecret'),
+                redirect_uri:   FB.options('redirectUri'),
+                code:           code
+            }, this);
+        },
+        function extendAccessToken(err, result) {
+            if(err) throw(err);
+            FB.napi('oauth/access_token', {
+                client_id:          FB.options('appId'),
+                client_secret:      FB.options('appSecret'),
+                grant_type:         'fb_exchange_token',
+                fb_exchange_token:  result.access_token
+            }, this);
+        },
+        function (err, result) {
+            if(err) return next(err);
+
+            req.session.access_token    = result.access_token;
+            req.session.expires         = result.expires || 0;
+
+            if(req.query.state) {
+                var parameters              = JSON.parse(req.query.state);
+                parameters.access_token     = req.session.access_token;
+
+                console.log(parameters);
+
+                FB.api('/me/friends', function(response) {
+		    res.send(response);
+		});
+            } else {
+                return res.redirect('/');
+            }
+        }
+    );
 };
 
 app.get( '/',                root);
