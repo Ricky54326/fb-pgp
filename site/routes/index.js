9192d9392d0c32ed8d00f86ee78f993ef7bb35e4
Zach Thomae <me@zthomae.com>
2017-02-12 20:43:24 -0600

    Remove html rendering from server

--- site/routes/index.js
+++ site/routes/index.js
@@ -2,15 +2,10 @@ const FB = require('fb');
 const login_callback = require('./callback');
 
 const index = appId => (req, res) => {
-  const accessToken = req.session.access_token;
   const loginUrl = FB.getLoginUrl({
     scope: 'user_friends, public_profile'
   });
-  res.render('index', {
-    appId,
-    loginUrl,
-    title: 'Pretty Good Messenger'
-  });
+  res.send(JSON.stringify({ loginUrl }));
 };
 
 module.exports = (app, appId) => {
