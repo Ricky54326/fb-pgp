7765bd149f2609e99799fbac53590193cf138b18
Zach Thomae <me@zthomae.com>
2017-02-12 21:25:49 -0600

    Revert "Remove html rendering from server"
    This reverts commit 9192d9392d0c32ed8d00f86ee78f993ef7bb35e4.

--- site/routes/index.js
+++ site/routes/index.js
@@ -3,10 +3,15 @@ const login_callback = require('./callback');
 const getKeys = require('./getKeys');
 
 const index = appId => (req, res) => {
+  const accessToken = req.session.access_token;
   const loginUrl = FB.getLoginUrl({
     scope: 'user_friends, public_profile'
   });
-  res.send(JSON.stringify({ loginUrl }));
+  res.render('index', {
+    appId,
+    loginUrl,
+    title: 'Pretty Good Messenger'
+  });
 };
 
 module.exports = (app, appId) => {
